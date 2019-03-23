a1d1c44ccc63488a74805480bc76b0d6cffbd856
Allen Tsai <allentsai87@gmail.com>
2019-03-23 11:58:57 -0700

    view engine comment

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import * as express from 'express';
 const app = express()
 const port = 3000
+
+// View Engine Setup - this is what allows views to "magically" work
 app.set('views', './src/views')
 app.set('view engine', 'pug')
+
 app.get('/', function (req, res) {
   res.render('index', { title: 'Hey', message: 'Hello there!' })
 });
